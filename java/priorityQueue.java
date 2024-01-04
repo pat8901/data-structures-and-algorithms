e1d161b07b35cea4f031657626a536f9db434254
Patrick <seitzpr@gmail.com>
2024-01-04 11:59:34 -0500

    Added new java data structures to make

---
+++ java/priorityQueue.java
@@ -0,0 +1,25 @@
+/*Priority queue can be implemented using an array, a linked list, a heap data structure, or a binary search tree. 
+Among these data structures, heap data structure provides an efficient implementation of priority queues.*/
+
+class PriorityQueue {
+
+    PriorityQueue() {
+        System.out.println("Hello World!");
+    }
+
+    public void peek() {
+
+    }
+
+    public void insert() {
+
+    }
+
+    public int delete() {
+
+    }
+
+    public void printHeap() {
+
+    }
+}
