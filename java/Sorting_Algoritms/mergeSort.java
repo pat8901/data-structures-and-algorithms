c2d1c6f5c305fff41ab44cc730c8eaaba6c95207
Patrick <95184328+pat8901@users.noreply.github.com>
2024-01-09 17:27:15 -0500

    Added java merge sort

--- java/Sorting_Algoritms/mergeSort.java
+++ java/Sorting_Algoritms/mergeSort.java
@@ -1,5 +1,34 @@
 package Sorting_Algoritms;
+import java.util.Random;
 
-public class mergeSort {
+class MergeSort {
+    private int array[];
+    int capacity;
+
+    MergeSort(int size){
+        capacity = size;
+        array = new int[capacity];
+    }
+
+    public void fillArray(){   
+        Random random_number = new Random();
+        for (int i = 0; i < array.length; i++){
+            array[i] = random_number.nextInt(20);
+        }
+
+    }
+    public void printArray(){
+        for(int i = 0; i < array.length-1; i++){
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println(array[array.length-1]);
+    }
+
+    public static void main(String args[]){
+        MergeSort merge = new MergeSort(10);
+        merge.printArray();
+        merge.fillArray();
+        merge.printArray();
+    }
 
 }
