5bdebdd27d75f88ca2efdbf977c5e0c944a2102e
Patrick Seitz <seitzpr@gmail.com>
2024-01-23 15:32:10 -0500

    Worked on minheap

--- java/Data_Structures/MinHeap.java
+++ java/Data_Structures/MinHeap.java
@@ -45,18 +45,64 @@ public class MinHeap {
         if (index >= length || leftIndex >= length) {
             return;
         }
+        int left_value = data[leftIndex];
+        int right_value = data[rightIndex];
+        int our_value = data[index];
 
+        if (left_value > right_value && our_value > right_value) {
+            data[index] = right_value;
+            data[rightIndex] = our_value;
+            heapifyDown(rightIndex);
+        }
+
+        if (right_value > left_value && our_value > left_value) {
+            data[index] = left_value;
+            data[leftIndex] = our_value;
+            heapifyDown(leftIndex);
+        }
     }
 
-    public boolean insert() {
-        return false;
+    public void insert(int value) {
+        data[length] = value;
+        heapifyUp(length);
+        length++;
     }
 
-    public boolean delete() {
-        return false;
+    public int delete() {
+        if (length == 0) {
+            return -1;
+        }
+
+        int value = data[0];
+        length--;
+
+        if (length == 0) {
+            data = new int[capacity];
+            return value;
+        }
+
+        data[0] = data[length];
+        heapifyDown(0);
+        return value;
     }
 
     public void peek() {
 
+    }
+
+    public void printHeap() {
+
+    }
+
+    public static void main(String args[]) {
+        MinHeap heap = new MinHeap(10);
+
+        heap.insert(20);
+        heap.insert(7);
+        heap.insert(3);
+        heap.insert(90);
+        heap.insert(1);
+        heap.insert(256);
+        System.out.println("Value: " + heap.delete());
     }
 }
